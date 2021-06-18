eb403037955f590b88c8a218e77218ef2d79379c
tseye <tserendorj.tumen@gmail.com>
2021-06-18 21:46:33 +0800

    tusviig delgetsend update hiine

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ var uiController = (function(){
         inputValue:".add__value",
         addBtn: '.add__btn',
         incomeList: '.income__list',
-        expenseList: '.expenses__list'
+        expenseList: '.expenses__list',
+        tusuvLabel: ".budget__value",
+        incomeLabel: ".budget__income--value",
+        expenseLabel: ".budget__expenses--value",
+        percentageLabel: ".budget__expenses--percentage"
     };
     
     return {
@@ -32,10 +36,18 @@ var uiController = (function(){
             });
 
             fieldsArr[0].focus();
+        },
 
-            // for(var i = 0; i < fieldsArr.length; i++){
-            //     fieldsArr[i].value = "";
-            // }
+        tusviigUzuuleh: function(tusuv){
+            document.querySelector(DOMstrings.tusuvLabel).textContent = tusuv.tusuv;
+            document.querySelector(DOMstrings.incomeLabel).textContent = tusuv.totalInc;
+            document.querySelector(DOMstrings.expenseLabel).textContent = tusuv.totalExp;
+            if (tusuv.huvi !== 0){
+                document.querySelector(DOMstrings.percentageLabel).textContent = tusuv.huvi + "%";
+            }else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = tusuv.huvi
+            }
+            
         },
 
         addListItem: function(item, type){
@@ -156,7 +168,7 @@ var appController = (function(uiController, financeController){
             // 5. calculate resiudal amount and display
             var tusuv = financeController.tusviigAvah();
 
-            console.log(tusuv);
+            uiController.tusviigUzuuleh(tusuv);
         }
         
     };
@@ -178,6 +190,12 @@ var appController = (function(uiController, financeController){
     return {
         init: function(){
             console.log('Application started...');
+            uiController.tusviigUzuuleh({
+                tusuv: 0,
+                huvi: 0,
+                totalInc: 0,
+                totalExp: 0
+            })
             setupEventListenters();
         }
     }
